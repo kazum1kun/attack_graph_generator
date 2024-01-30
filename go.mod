bf005a091f6f3b67136dd0a1a2c5a15d197c20df
Xuanli Lin <l19980623@gmail.com>
2024-01-30 12:46:03 -0700

    initial commit with working shell

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/kazum1kun/attack_graph_generator
+
+go 1.21
+
+require github.com/urfave/cli/v2 v2.27.1
---
+++ main.go
@@ -0,0 +1,117 @@
+package main
+
+import (
+	"fmt"
+	"github.com/urfave/cli/v2"
+	"log"
+	"os"
+	"time"
+)
+
+func main() {
+	app := &cli.App{
+		Flags: []cli.Flag{
+			&cli.IntSliceFlag{
+				Name:     "node",
+				Usage:    "number of PF, AND, and OR nodes",
+				Aliases:  []string{"n"},
+				Required: true,
+				Action: func(ctx *cli.Context, input []int) error {
+					haveNeg := false
+					for _, v := range input {
+						if v <= 0 {
+							haveNeg = false
+						}
+					}
+					if len(input) != 3 || haveNeg {
+						return fmt.Errorf("flag node should contain exactly 3 positive integers")
+					}
+					return nil
+				},
+			},
+			&cli.IntFlag{
+				Name:     "edge",
+				Usage:    "number of edges",
+				Aliases:  []string{"e"},
+				Required: true,
+				Action: func(ctx *cli.Context, i int) error {
+					if i <= 0 {
+						return fmt.Errorf("flag edge must be positive")
+					}
+					return nil
+				},
+			},
+			&cli.BoolFlag{
+				Name:        "cycle",
+				Usage:       "whether cycles are permitted (does not guarantee them)",
+				Aliases:     []string{"c"},
+				Value:       false,
+				DefaultText: "false",
+			},
+			&cli.Int64Flag{
+				Name:        "seed",
+				Usage:       "random seed",
+				Aliases:     []string{"s"},
+				Value:       time.Now().Unix(),
+				DefaultText: "current Unix epoch in seconds",
+			},
+			&cli.StringFlag{
+				Name:        "outdir",
+				Usage:       "output to `DIR`",
+				Value:       ".",
+				Aliases:     []string{"o"},
+				DefaultText: "current directory",
+				Action: func(ctx *cli.Context, s string) error {
+					_, err := os.Stat("s")
+					if os.IsNotExist(err) {
+						return fmt.Errorf("%v does not exist", s)
+					}
+					return nil
+				},
+			},
+			&cli.BoolFlag{
+				Name:        "graph",
+				Usage:       "generate a pdf rendition of the attack graph",
+				Aliases:     []string{"g"},
+				Value:       false,
+				DefaultText: "false",
+			},
+			&cli.BoolFlag{
+				Name:        "graph",
+				Usage:       "generate a pdf rendition of the attack graph",
+				Aliases:     []string{"g"},
+				Value:       false,
+				DefaultText: "false",
+			},
+			&cli.BoolFlag{
+				Name:        "nocheck",
+				Usage:       "disable basic input validation checks (may result in invalid graphs)",
+				Value:       false,
+				DefaultText: "false",
+			},
+		},
+		Action: func(ctx *cli.Context) error {
+			// PF AND OR
+			nodeNum := ctx.IntSlice("node")
+			edgeNum := ctx.Int("edge")
+
+			if !ctx.Bool("nocheck") {
+				// Sanity check
+				minEdge := (nodeNum[0] + 2*nodeNum[1] + nodeNum[2]) / 2
+				maxEdge := (2*nodeNum[0] + nodeNum[2] + 1) * nodeNum[1] / 2
+				if edgeNum < minEdge || edgeNum > maxEdge {
+					return fmt.Errorf("flag edge out of bound for current input, valid range [%v-%v], current %v",
+						minEdge, maxEdge, edgeNum)
+				}
+			}
+			return nil
+		},
+	}
+	if err := app.Run(os.Args); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func constructGraph(leaf, and, or, edge int, cycle bool) {
+
+}
